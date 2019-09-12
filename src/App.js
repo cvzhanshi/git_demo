ca8ff2cb8072f78eb832ca458c60d7f0192351d7
Jerry123-ctrl <13504483348@139.com>
2019-09-12 14:21:37 +0800

    第一次提交

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function App(){
+  return (
+    <div>
+      <h3>react-redux</h3>
+    </div>
+  )
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//index.js文件
+// import Store from './react-redux-store/store'
+// import {Provider} from 'react-redux' //Provider将state注册到根组件，
+//让根组件下的所有的组件直接获取到store对象
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render( <App />
+    ,document.getElementById('root'));
+
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
